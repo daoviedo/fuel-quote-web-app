6f10b8fd9f9905bb5de89b4da1731a18cef6fa79
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-02 03:46:46 -0600

    Redesigned the navBar

--- my-app/src/pages/components/nav_bar.js
+++ my-app/src/pages/components/nav_bar.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
-import { Button, Nav, NavDropdown, Navbar, Form, FormControl } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
 import './nav_bar.css';
 
-export default class Home extends Component {
+import AppBar1 from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+import Settings from '@material-ui/icons/Settings';
+import History from '@material-ui/icons/History';
+import Exit from '@material-ui/icons/ExitToApp'
+import Logo from '@material-ui/icons/Camera';
+import AdminLogo from '@material-ui/icons/VerifiedUser';
+import UserLogo from '@material-ui/icons/AccountCircle';
+
+import {Link} from 'react-router-dom';
+
+
+import { Drawer, List, ListItem, ListItemText, Divider,} from "@material-ui/core";
+
+const styles = _ =>({
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    bar: {
+        backgroundColor: "#00897b"
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+});
+
+class AppBar extends Component {
     constructor(props) {
         super(props);
     
@@ -12,6 +46,7 @@ export default class Home extends Component {
                 username: localStorage.getItem("username"),
                 password: "ENCRYPTED"
             },
+            openUserMenu: false
         };
     }
 
@@ -19,26 +54,64 @@ export default class Home extends Component {
         localStorage.clear();
         this.setState({loggedInn: false, user: {username: "", password: ""}});
     }
+    
     render() {
+        const { classes } = this.props;
         const finalLogged = this.state.loggedInn;
         return (
-            <Navbar bg="dark" expand="lg">
-                <Navbar.Brand href="#home"><h4>Devry Energy</h4></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/"><h5>Home</h5></Nav.Link>
-                        {finalLogged ? <Nav.Link href="/userlist"><h5>Admin</h5></Nav.Link>: <div/>}
-                        {finalLogged ? <Nav.Link href="/req_fuel_quote"><h5>Request Fuel Quote</h5></Nav.Link>: <div/>}
-                    </Nav>
-                    {finalLogged ? (<NavDropdown title="My Account" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="manage_account">Edit Profile</NavDropdown.Item>
-                        <NavDropdown.Item href="/fuel_history">Quote History</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item onClick={this.logOff.bind(this)} href="/">Log out</NavDropdown.Item>
-                        </NavDropdown>) : (<Button size="sm" href="/login">Log-in</Button> )}
-                </Navbar.Collapse>
-            </Navbar>
+            <div className={classes.root}>
+                <AppBar1 position="static" className={classes.bar}>
+                    <Toolbar>
+                        <Logo style={{fontSize:"250%", marginRight:4}}/>
+                        <Typography variant="h4" style={{marginBottom:2}} align="left" color="inherit" className={classes.grow}>Devry Energy</Typography>
+                        {finalLogged ? 
+                        (<div>
+                            <Drawer anchor="right" open={this.state.openUserMenu} onClose={()=>this.setState({openUserMenu: false})}>
+                                <div onClick={()=> this.setState({openUserMenu: false})}>
+                                    <List>
+                                        <Link to="/manage_account" style={{ textDecoration: 'none'}}>
+                                        <ListItem button>
+                                            <Settings style={{color: "#00897b"}}/>
+                                            <ListItemText primary='Account Settings' />
+                                        </ListItem>
+                                        </Link>
+                                        <Link to="/fuel_history" style={{ textDecoration: 'none'}}>
+                                        <ListItem button>
+                                            <History style={{color: "#00897b"}}/>
+                                            <ListItemText primary='Fuel Quote History' />
+                                        </ListItem>
+                                        </Link>
+                                        <Link to="/userlist" style={{ textDecoration: 'none'}}>
+                                        <ListItem button>
+                                            <AdminLogo style={{color: "#00897b"}}/>
+                                            <ListItemText primary='Admin'/>
+                                        </ListItem>
+                                        </Link>
+                                    </List>
+                                    <Divider/>
+                                    
+                                    <List>
+                                        <Link to="/" style={{ textDecoration: 'none'}}>
+                                        <ListItem button onClick={this.logOff.bind(this)}>
+                                            <Exit style={{color: "#00897b"}}/>
+                                            <ListItemText primary='Log out' />
+                                        </ListItem>
+                                        </Link>
+                                    </List>
+                                </div>
+                            </Drawer>
+                            <Button color="inherit" style={{textTransform: 'none', outline: 0, fontSize: "90%"}} onClick={()=> this.setState({openUserMenu: true})}><UserLogo style={{marginRight: 3,fontSize: "180%"}}/>{this.state.user.username}</Button></div>) : 
+                        (<Button color="inherit" style={{outline: 0}} href="/login">Login</Button>)}
+                        
+                    </Toolbar>
+                </AppBar1>
+            </div>
         );
     }
 }
+
+AppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+  
+export default withStyles(styles)(AppBar);
