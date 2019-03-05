e6a7f97da6693782574c3debf234abbba40238b7
JoelJijo <joeljijo123>
2019-03-04 21:36:41 -0600

    added beginning of history components

--- my-app/src/pages/components/history_table.js
+++ my-app/src/pages/components/history_table.js
@@ -3,6 +3,36 @@ import './history_table.css';
 import Table from 'react-bootstrap/Table'
 
 export default class HistoryTable extends Component {
+    componentDidMount(){
+        this.getDataFromHistory();
+    }
+    state = {
+        username:localStorage.getItem("username"),
+        Requests: [],
+    }
+    getDataFromHistory = () =>{
+        fetch(`http://localhost:4000/users/history?username=${this.state.username}`)
+        .then(Response => Response.json())
+        .then(Response => this.setState({Requests: Response.data}))
+        .catch(err => console.log(err))
+    }
+    getDataFromUser = () =>{
+        fetch(`http://localhost:4000/users/fuelrequestinfo?username=${this.state.username}`)
+        .then(Response => Response.json())
+        .then(Response => this.setState({ad1: Response.data[0].city}))
+        .catch(err => console.log(err))
+    }
+    
+    renderUser = ({GallonsRequested, PricePerGallon, TotalPrice, DeliveryAddress, DeliveryCity, DeliveryState, DeliveryZip, DeliveryDate, DateOfRequest}) =>
+    <tr> 
+        <td>{GallonsRequested}</td>
+        <td>{PricePerGallon}</td>
+        <td>{TotalPrice}</td> 
+        <td>{DeliveryAddress}, {DeliveryCity}, {DeliveryState} {DeliveryZip}</td> 
+        <td>{DateOfRequest}</td> 
+        <td>{DeliveryDate}</td> 
+    </tr>
+
     render() {
         return (
             <div className="table-container">
@@ -18,8 +48,12 @@ export default class HistoryTable extends Component {
                         </tr>
                     </thead>
                     <tbody className="react-bootstrap-table-body">
+                        {this.state.Requests.map(this.renderUser)}
+                    </tbody>
+                    <tbody className="react-bootstrap-table-body">
                         <tr>
-                            <td>07/18/2018</td>
+
+                            <td></td>
                             <td>1235</td>
                             <td>2152 Calhoun Rd</td>
                             <td>07/21/2018</td>
--- node/index.js
+++ node/index.js
@@ -98,6 +98,21 @@ app.get('/users/check', (req, res) => {
     });
 });
 
+app.get('/users/fuelrequestinfo', (req, res) => {
+    const { username } = req.query;
+    const addressQuery = `SELECT DeliveryAddress, DeliveryCity, DeliveryState, DeliveryZip FROM sys.history WHERE username='${username}'`;
+    connection.query(addressQuery, (err, results) => {
+        if(err){
+            return res.send(err)
+        }
+        else{
+            return res.json({
+                data: results
+            })
+        }
+    });
+});
+
 app.get('/users', (req, res) => {
     connection.query(selectAll, (err, results) => {
         if(err){
@@ -110,6 +125,43 @@ app.get('/users', (req, res) => {
         }
     });
 });
+
+app.get('/users/history', (req, res) => {
+    const { username } = req.query;
+    const findHistory = `SELECT * FROM sys.history WHERE username='${username}'`;
+    connection.query(findHistory, (err, results) => {
+        if(err){
+            return res.send(err)
+        }
+        else{
+            return res.json({
+                data: results
+            })
+        }
+    });
+});
+
+app.get('/users/addRequest', (req, res) => {
+    var date= new Date();
+    date = date.getUTCFullYear() + '-' +
+    ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
+    ('00' + date.getUTCDate()).slice(-2) + ' ' + 
+    ('00' + date.getUTCHours()).slice(-2) + ':' + 
+    ('00' + date.getUTCMinutes()).slice(-2) + ':' + 
+    ('00' + date.getUTCSeconds()).slice(-2);
+    const { UserName, GallonsRequested, PricePerGallon } = req.query;
+    const insertQuery = `INSERT INTO sys.history (username, GallonsRequested, PricePerGallon, TotalPrice, DateOfRequest) 
+                        VALUES('${UserName}','${GallonsRequested}','${PricePerGallon}','${PricePerGallon*GallonsRequested}','${date}')`;
+    connection.query(insertQuery, (err, results) => {
+        if(err){
+            return res.send(err)
+        }
+        else{
+            return res.send('Successfully Added History')
+        }
+    });
+});
+
 
 app.listen(4000, () => {
     console.log(`Server listening on port 4000`)
