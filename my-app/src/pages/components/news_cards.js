78cff3f7e223dfb74131a43110136e3374214450
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-05 17:57:03 -0600

    date change

--- my-app/src/pages/components/news_cards.js
+++ my-app/src/pages/components/news_cards.js
@@ -56,7 +56,7 @@ class news_cards_test extends Component {
     getData = _ => {
         var holder =[];
         const API_KEY = 'a822076091e14f77a0ef87bc40c8aaf6';
-        let url = 'https://newsapi.org/v2/everything?q=petroleum&from=2019-03-05&sortBy=publishedAt&apiKey=' + API_KEY;
+        let url = 'https://newsapi.org/v2/everything?q=petroleum&from=2019-02-25&sortBy=publishedAt&apiKey=' + API_KEY;
         fetch(url)
             .then((response) => response.json())
             .then((responseJson) => {
