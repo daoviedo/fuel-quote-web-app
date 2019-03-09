da490a1274793b28934d58c7e17fbd9ef68e4345
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-09 01:03:13 -0600

    testing

--- my-app/src/pages/Acc_mng.js
+++ my-app/src/pages/Acc_mng.js
@@ -156,12 +156,14 @@ class Acc_mng extends Component{
         const user = localStorage.getItem("username");
         fetch(`http://138.197.221.30:4000/users/data/${user}`)
         .then(response => response.json())
-        .then(res => this.setState({firstName: res.data[0].firstname, lastName: res.data[0].lastname, address1: res.data[0].ad1, address2: res.data[0].ad2, city: res.data[0].city, dropSelection: res.data[0].st, zip: res.data[0].zip}))
+        .then(res => this.setState({firstName: res.data[0].firstname, lastName: res.data[0].lastname, address1: res.data[0].ad1, address2: res.data[0].ad2, city: res.data[0].city, dropSelection: res.data[0].st, zip: res.data[0].zip, needsUpdate: false}))
         .catch(err => console.log(err))
     }
 
     updateProfile = _ => {
-        
+        fetch(`http://138.197.221.30:4000/users/update/${user}?f=${this.state.firstName}&l=${this.state.lastName}&a1=${this.state.address1}&a2=${this.state.address2}&c=${this.state.city}&s=${this.state.dropSelection}&z=${this.state.zip}`)
+        .then(this.fetchData())
+        .catch(err => console.log(err))
     }
     
     render(){
