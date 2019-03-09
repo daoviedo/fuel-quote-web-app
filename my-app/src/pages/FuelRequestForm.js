4a9771cb72c03b8c2dd0b29b07e87b649e20d8a4
JoelJijo <joeljijo123>
2019-03-08 22:24:33 -0600

    asa

--- my-app/src/pages/FuelRequestForm.js
+++ my-app/src/pages/FuelRequestForm.js
@@ -94,17 +94,7 @@ class FuelRequestForm extends React.Component{
     render() {
         const {classes}=this.props;
         return (
-            <main className={classes.layout}> 
-                <Paper className={classes.root} elevation={1}>
-                    <FormControl fullWidth className={classes.margin}>
-                        <InputLabel>Amount of Fuel desired</InputLabel>
-                        <Input
-                        value={this.state.GallonsRequested}
-                        onChange={e => this.change(e)}
-                        />
-                    </FormControl>
-                </Paper>
-            </main>
+            <div>hello</div>
         );
         
     }
