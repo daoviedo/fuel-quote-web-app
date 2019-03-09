21a2530684e459b9974031419d3203ee01d53660
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-08 22:36:48 -0600

    testing acc

--- my-app/src/pages/Acc_mng.js
+++ my-app/src/pages/Acc_mng.js
@@ -1,60 +1,173 @@
 import React, { Component } from "react";
-import {Button,Form,Col} from "react-bootstrap";
-import "./css/Acc_mng.css";
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const options = [
+    'AL',
+    'AK',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'DC',
+    'FL',
+    'GA',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'OH',
+    'OK',
+    'OR',
+    'PA',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY'
+  ];
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+
+const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      },
+    },
+};
+
 
 
 export default class Acc_mng extends Component{
+    
     render(){
         return(
-            <div className="Acc_mng">
-                <Form>
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="formGridFirst">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control placeholder="Ex. John" />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridLast">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control placeholder="Ex. Smith" />
-                        </Form.Group>
-                    </Form.Row>
-
-                    <Form.Group controlId="formGridAddress1">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control placeholder="Ex. 1234 Main St" />
-                    </Form.Group>
-
-                    <Form.Group controlId="formGridAddress2">
-                        <Form.Label>Address 2</Form.Label>
-                        <Form.Control placeholder="Apartment, studio, or floor" />
-                    </Form.Group>
-
-                    <Form.Row>
-                        <Form.Group as={Col} controlId="formGridCity">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>State</Form.Label>
-                        <Form.Control as="select">
-                            <option>Choose...</option>
-                            <option>...</option>
-                        </Form.Control>
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridZip">
-                        <Form.Label>Zip</Form.Label>
-                        <Form.Control />
-                        </Form.Group>
-                    </Form.Row>
-
-                    <Button variant="primary" type="submit" href="/">
-                        Submit
-                    </Button>
-                    </Form>
-            </div>
+            
+            <React.Fragment>
+                <Paper>
+              <Typography variant="h6" gutterBottom>
+                Complete your profile
+              </Typography>
+              <Grid container spacing={24}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    id="firstName"
+                    name="firstName"
+                    label="First name"
+                    fullWidth
+                    autoComplete="fname"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    id="lastName"
+                    name="lastName"
+                    label="Last name"
+                    fullWidth
+                    autoComplete="lname"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    id="address1"
+                    name="address1"
+                    label="Address line 1"
+                    fullWidth
+                    autoComplete="billing address-line1"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="address2"
+                    name="address2"
+                    label="Address line 2"
+                    fullWidth
+                    autoComplete="billing address-line2"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    required
+                    id="city"
+                    name="city"
+                    label="City"
+                    fullWidth
+                    autoComplete="billing address-level2"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                <FormControl>
+                    <InputLabel required>State</InputLabel>
+                    <Select
+                        value={this.props.val.dropSelection}
+                        style={{minWidth: 120}}
+                        MenuProps={MenuProps}
+                        inputProps={{
+                        name: 'dropSelection',
+                        }}
+                    >
+                        <MenuItem value="">
+                        <em>None</em>
+                        </MenuItem >
+                        {options.map(opp => (
+                            <MenuItem value={opp} key={opp}>{opp}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    required
+                    id="zip"
+                    name="zip"
+                    label="Zip / Postal code"
+                    fullWidth
+                    autoComplete="billing postal-code"
+                  />
+                </Grid>
+              </Grid>
+              </Paper>
+            </React.Fragment>
         );
     }
 }
