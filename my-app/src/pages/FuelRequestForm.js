004dab5c8619988355a379c1cdfc79f99360a2de
JoelJijo <joeljijo123>
2019-03-08 21:12:14 -0600

    updated todo

--- my-app/src/pages/FuelRequestForm.js
+++ my-app/src/pages/FuelRequestForm.js
@@ -66,85 +66,90 @@ export default class FuelRequestForm extends React.Component{
     }
 
     render() {
+        const {classes}=this.props;
         return (
-            <div className="FuelRequestForm">
-            <div><Navbar /></div>
-                <header className="FuelRequestForm-header">
-                    <Form>
-                        <Form.Group as={Row} controlId="formGallonsRequested">
-                            <Form.Label sm={3} >
-                                Gallons Requested
-                            </Form.Label>
-                            <Form.Control 
-                            name="GallonsRequested"
-                            value={this.state.GallonsRequested}
-                            onChange={e => this.change(e)}
-                            size="xs" 
-                            type="number"
-                            />
-                        </Form.Group>
+            <React.Fragment>
+                <main className={classes.layout}>
+                </main>
+            </React.Fragment>
+            // <div className="FuelRequestForm">
+            // <div><Navbar /></div>
+            //     <header className="FuelRequestForm-header">
+            //         <Form>
+            //             <Form.Group as={Row} controlId="formGallonsRequested">
+            //                 <Form.Label sm={3} >
+            //                     Gallons Requested
+            //                 </Form.Label>
+            //                 <Form.Control 
+            //                 name="GallonsRequested"
+            //                 value={this.state.GallonsRequested}
+            //                 onChange={e => this.change(e)}
+            //                 size="xs" 
+            //                 type="number"
+            //                 />
+            //             </Form.Group>
 
-                        <Form.Group as={Row} controlId="formStreetAddress">
-                            <Form.Label>Delivery Address</Form.Label>
-                            <Form.Control  
-                            value={this.state.DeliveryAddress1}
-                            disabled />
-                            <Form.Label>Delivery Address2</Form.Label>
-                            <Form.Control  
-                            value={this.state.DeliveryAddress2}
-                            disabled />
-                            <Form.Group as={Col} controlId="formAddressCity">
-                                <Form.Label>City</Form.Label>
-                                <Form.Control disabled 
-                                placeholder={this.state.DeliveryCity}/>
-                            </Form.Group>
+            //             <Form.Group as={Row} controlId="formStreetAddress">
+            //                 <Form.Label>Delivery Address</Form.Label>
+            //                 <Form.Control  
+            //                 value={this.state.DeliveryAddress1}
+            //                 disabled />
+            //                 <Form.Label>Delivery Address2</Form.Label>
+            //                 <Form.Control  
+            //                 value={this.state.DeliveryAddress2}
+            //                 disabled />
+            //                 <Form.Group as={Col} controlId="formAddressCity">
+            //                     <Form.Label>City</Form.Label>
+            //                     <Form.Control disabled 
+            //                     placeholder={this.state.DeliveryCity}/>
+            //                 </Form.Group>
 
 
-                            <Form.Group as={Col} controlId="formGridState">
-                                <Form.Label>State</Form.Label>
-                                <Form.Control disabled 
-                                placeholder={this.state.DeliveryState}/>
-                            </Form.Group>
+            //                 <Form.Group as={Col} controlId="formGridState">
+            //                     <Form.Label>State</Form.Label>
+            //                     <Form.Control disabled 
+            //                     placeholder={this.state.DeliveryState}/>
+            //                 </Form.Group>
 
-                            <Form.Group as={Col} controlId="formGridZip">
-                                <Form.Label>Zip</Form.Label>
-                                <Form.Control disabled 
-                                placeholder={this.state.DeliveryZip}/>
-                            </Form.Group>
-                        </Form.Group>
+            //                 <Form.Group as={Col} controlId="formGridZip">
+            //                     <Form.Label>Zip</Form.Label>
+            //                     <Form.Control disabled 
+            //                     placeholder={this.state.DeliveryZip}/>
+            //                 </Form.Group>
+            //             </Form.Group>
 
-                        <Form.Group as={Row} controlId="formDeliveryDate">
-                            <Form.Label> 
-                                Delivery Date    
-                            </Form.Label> 
-                        </Form.Group>
-                        <Form.Group as={Row} controlId="formDeliveryDate">
-                            <DatePicker  
-                                minDate={new Date()} 
-                                selected={this.state.DeliveryDate} 
-                                onChange={this.dateChanged} 
-                                onSelect={this.handleSelect} 
-                                value={this.state.DeliveryDate}
-                            />
-                        </Form.Group>
-                        <Form.Group as={Row}>
-                            <Form.Group as={Col}>
-                                <Form.Label>Suggested Price</Form.Label>
-                                <Form.Control disabled type="number" value={this.state.SuggestedPrice}
-                                />
-                            </Form.Group>
-                            <Form.Group as={Col}>
-                                <Form.Label>Total Amount</Form.Label>
-                                <Form.Control disabled type="number" value={this.state.GallonsRequested*this.state.SuggestedPrice}
-                                />
-                            </Form.Group>
-                        </Form.Group>
-                        <Button variant="primary" onClick={()=>this.addRequestToHistory()}>
-                            Submit
-                        </Button>
-                    </Form>
-                </header>
-            </div>
+            //             <Form.Group as={Row} controlId="formDeliveryDate">
+            //                 <Form.Label> 
+            //                     Delivery Date    
+            //                 </Form.Label> 
+            //             </Form.Group>
+            //             <Form.Group as={Row} controlId="formDeliveryDate">
+            //                 <DatePicker  
+            //                     minDate={new Date()} 
+            //                     selected={this.state.DeliveryDate} 
+            //                     onChange={this.dateChanged} 
+            //                     onSelect={this.handleSelect} 
+            //                     value={this.state.DeliveryDate}
+            //                 />
+            //             </Form.Group>
+            //             <Form.Group as={Row}>
+            //                 <Form.Group as={Col}>
+            //                     <Form.Label>Suggested Price</Form.Label>
+            //                     <Form.Control disabled type="number" value={this.state.SuggestedPrice}
+            //                     />
+            //                 </Form.Group>
+            //                 <Form.Group as={Col}>
+            //                     <Form.Label>Total Amount</Form.Label>
+            //                     <Form.Control disabled type="number" value={this.state.GallonsRequested*this.state.SuggestedPrice}
+            //                     />
+            //                 </Form.Group>
+            //             </Form.Group>
+            //             <Button variant="primary" onClick={()=>this.addRequestToHistory()}>
+            //                 Submit
+            //             </Button>
+            //         </Form>
+            //     </header>
+            // </div>
         )
         
     }
