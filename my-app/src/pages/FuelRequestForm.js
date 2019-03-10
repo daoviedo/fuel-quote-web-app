7b733b487eb410b31e22baa244d991d0c5090445
JoelJijo <joeljijo123>
2019-03-10 14:58:10 -0500

    form with proper zip

--- my-app/src/pages/FuelRequestForm.js
+++ my-app/src/pages/FuelRequestForm.js
@@ -217,7 +217,7 @@ class FuelRequestForm extends Component{
                         label="Zip"
                         name= "DeliveryZip"
                         disabled
-                        value={this.state.OrderID}
+                        value={this.state.zip}
                         className={classes.AddMargin}
                     />
                     <TextField
