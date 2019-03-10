b4aa0625f2a851649798983ab43f95810a8e50ed
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-10 09:57:05 -0500

    fixed

--- node/index.js
+++ node/index.js
@@ -78,7 +78,7 @@ app.post('/test', (req,res,next)=>{
                             username: results[0].username,
                             password: results[0].password,
                             privelege: results[0].priv }, privateKey, { expiresIn: "1h" }, function(err2, token) {
-                                bcrypt.hash(myPlaintextPassword, saltRounds, function(err3, hash) {
+                                bcrypt.hash(token, saltRounds, function(err3, hash) {
                                     return res.json({
                                         data: {
                                             authentication: true,
