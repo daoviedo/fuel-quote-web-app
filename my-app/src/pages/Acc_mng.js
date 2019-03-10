5e0082ca960ccae54209ba223c5c2fe46ed7e598
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-10 13:26:06 -0500

    finalized accmng

--- my-app/src/pages/Acc_mng.js
+++ my-app/src/pages/Acc_mng.js
@@ -178,7 +178,6 @@ class Acc_mng extends Component{
     }
 
     updateProfile() {
-        console.log(this.state.username)
         const user = this.state.username;
         fetch(`http://138.197.221.30:4000/users/update/${user}?f=${this.state.firstName}&l=${this.state.lastName}&a1=${this.state.address1}&a2=${this.state.address2}&c=${this.state.city}&s=${this.state.dropSelection}&z=${this.state.zip}`)
         .then(this.fetchData())
