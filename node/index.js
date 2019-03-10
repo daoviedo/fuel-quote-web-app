528c952c5f18977fb921074bac941c175301a88a
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-09 21:36:41 -0600

    testing access

--- node/index.js
+++ node/index.js
@@ -24,8 +24,21 @@ connection.connect(err => {
     }
 });
 
-app.use(bodyParser.json());
+
 app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if(req.method==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 
 
 app.get('/', (req, res) => {
