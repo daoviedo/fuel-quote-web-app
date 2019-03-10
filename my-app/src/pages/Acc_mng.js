0d83533171413fde51864638b32c2821ac7f6471
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-10 13:36:54 -0500

    bug fix

--- my-app/src/pages/Acc_mng.js
+++ my-app/src/pages/Acc_mng.js
@@ -151,7 +151,7 @@ class Acc_mng extends Component{
         this.verifyData();
     }
 
-    handleChange(event) {
+    handleChange = event => {
         this.setState({
           [event.target.name]: event.target.value, needsUpdate: true
         });
