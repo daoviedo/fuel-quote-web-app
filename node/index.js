4bd635e4ace7bb369b0cc63a6ddb8f6bce215fbe
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-10 00:52:05 -0600

    test

--- node/index.js
+++ node/index.js
@@ -30,7 +30,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
     if(req.method==='OPTIONS'){
