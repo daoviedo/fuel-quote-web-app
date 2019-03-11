ca7f8806dac0dd0e0ec623ff1043baa4b2c1b608
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-11 01:01:01 -0500

    fixed bug

--- my-app/src/pages/components/home_carousel.js
+++ my-app/src/pages/components/home_carousel.js
@@ -3,8 +3,8 @@ import { Carousel } from 'react-bootstrap';
 
 import './car.css';
 import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
+import image2 from './images/image2.jpeg';
+import image3 from './images/image3.jpeg';
 
 
 export default class Home extends Component {
