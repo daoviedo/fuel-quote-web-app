955b2e44604c43c1aed5224917642096c0f2690b
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-11 16:58:44 -0500

    open backend

--- node/index.js
+++ node/index.js
@@ -35,7 +35,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "http://www.fuelrequest.ga, fuelrequest.ga");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
     if(req.method==='OPTIONS'){
