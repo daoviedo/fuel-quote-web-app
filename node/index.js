ffb8c2bd8beaf559e7c9944d7829946cc487a0f1
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-17 15:43:37 -0500

    testing

--- node/index.js
+++ node/index.js
@@ -205,7 +205,7 @@ app.get('/users/data', checkAuth, (req, res) => {
 //Refactor this using checkAuth
 app.put('/users/update', checkAuth, (req, res) => {
     const username  = req.userData.username;
-    console.log(req.body.firstname)
+    console.log(username)
     const addressQuery = `UPDATE sys.user SET firstname='${req.body.firstname}',lastname='${req.body.lastname}',ad1='${req.body.address1}',ad2='${req.body.address2}',city='${req.body.city}',st='${req.body.st}',zip='${req.body.zip}' WHERE username='${username}'`;
     connection.query(addressQuery, (err, results) => {
         if(err){
