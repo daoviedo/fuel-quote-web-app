002a2a4d1ca7c7f32c0920ffdb6cec3fdbd809cb
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-17 15:35:45 -0500

    testing change

--- my-app/src/pages/Acc_mng.js
+++ my-app/src/pages/Acc_mng.js
@@ -177,7 +177,7 @@ class Acc_mng extends Component{
       .catch(err => console.log(err))
     }
 
-    patchProfile = _ =>{
+    patchProfile(){
       fetch(`http://138.197.221.30:4000/users/update`,{
           method: "PATCH",
           headers: {
