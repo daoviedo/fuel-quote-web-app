f7df60570f6afb44dfc26fbd447ef7dc14ccc1f6
Shahzaib Ali <shahzaibsa98@gmail.com>
2019-03-21 13:42:05 -0500

    added for loop

--- node/test/test.js
+++ node/test/test.js
@@ -5,16 +5,20 @@ let should = chai.should();
 
 chai.use(chaiHttp)
 
-describe('/GET username', () => {
-    it('it should GET the username', (done) => {
-      chai.request(server)
-          .get('/users/check?username=Daniel')
-          .end((err, res) => {
-                res.should.have.status(200);
-                res.body.data.should.be.a('array');
-                res.body.data.length.should.be.eql(1);
-                //res.body.length.should.be.eql(0);
-            done();
-          });
+const usernames = ['Daniel', 'Shahzaib', 'Joely'];
+
+for (let x = 0; x < usernames.length; x++) {
+    describe('/GET username', () => {
+        it('it should GET the username', (done) => {
+            chai.request(server)
+                .get('/users/check?username=' + usernames[x])
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.data.should.be.a('array');
+                    //res.body.data.length.should.be.eql(1);
+                    //res.body.length.should.be.eql(0);
+                    done();
+                });
+        });
     });
-});
+}
