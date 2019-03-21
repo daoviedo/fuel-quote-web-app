8acd1aa318c84ff4a9a1e84e4f9108efd0179c0f
Shahzaib Ali <shahzaibsa98@gmail.com>
2019-03-21 12:48:03 -0500

    added dependencies

---
+++ node/test/test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const chai = require('chai');
+const expect = require('chai').expect;
+
+chai.use(require('chai-http'));
+
+const app = require('../index.js');
+
+describe('Check users', function () {
+    it('should return user', function () {
+        return chai.request(app)
+            .get('/users/check?username=' + 'Daniel')
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.an('object');
+            });
+    });
+});
