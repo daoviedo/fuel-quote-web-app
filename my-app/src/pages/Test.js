5ed9c9a8e7e097bdcea71995c4090fcb710e3922
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-21 23:13:50 -0500

    test page

--- my-app/src/pages/Test.js
+++ my-app/src/pages/Test.js
@@ -1,88 +1,93 @@
-import React, { Component } from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import Navbar from './components/nav_bar';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-
-
 const styles = theme => ({
-    
-  });
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
+});
 
-class Test extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            logged: false,
-            rendered: false
-        }
-    }
+function SignIn(props) {
+  const { classes } = props;
 
-    componentDidMount(){
-        this.verifyData();
-    }
-
-    verifyData(){
-        fetch(`http://138.197.221.30:4000/verify`,{
-            method: "GET",
-            headers: {
-                "Authorization": "Bearer "+ document.cookie.split('=')[1]
-            }
-        })
-        .then(res => res.json())
-        .then(result => this.setState({logged: result.authentication, rendered: true}))
-        .catch(err => console.log(err))
-    }
-
-    handleUser = event =>{
-        this.setState({[event.target.name]: event.target.value});
-    }
-
-    render() {
-        //const { classes } = this.props;
-        if(this.state.rendered){
-            console.log(this.state.logged);
-        }
-        
-        return (
-            <div>  
-                <Navbar />
-                <br/><br/><br/><br/>
-                <TextField
-                        required
-                        id="username"
-                        name="username"
-                        label="Username"
-                        value={this.state.username}
-                        onChange={this.handleUser}
-                        style={{width: "35%",paddingBottom:"8px"}}
-                        margin="dense"
-                    />
-                   
-                    <br/>
-                    <TextField
-                        required
-                        id="password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleUser}
-                        label="Password"
-                        style={{width: "35%",paddingBottom:"30px"}}
-                        type="password"
-                        margin="dense"
-                    />
-                    <br/>
-                    <Button onClick={()=>this.getData()}>Submit</Button>
-                    <Button onClick={()=>this.verifyData()}>Verify</Button>
-            </div>
-        );
-    }
+  return (
+    <main className={classes.main}>
+      <CssBaseline />
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <Input id="email" name="email" autoComplete="email" autoFocus />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input name="password" type="password" id="password" autoComplete="current-password" />
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign in
+          </Button>
+        </form>
+      </Paper>
+    </main>
+  );
 }
 
-Test.propTypes = {
-    classes: PropTypes.object.isRequired,
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
-  
-export default withStyles(styles)(Test);
+
+export default withStyles(styles)(SignIn);
