c7e37d4be90d3072475b40dceccdfc28dd707fe5
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-03-21 20:50:11 -0500

    testing output

--- node/test/test.js
+++ node/test/test.js
@@ -119,7 +119,7 @@ describe('/DELETE delete user with valid authentication', () => {
             .set("Authorization", "Bearer "+ token)
             .end((err, res) => {
                 res.should.have.status(200);
-                console.log(res.body);
+                console.log(res);
                 done();
             });
     });
